8059d24717f52f72ecf902c18f352a296ae0068d
Cyunrei <2067357759@qq.com>
2018-03-17 08:54:40 +0800

    Add files via upload

---
+++ encryption/ContrastList.java
@@ -0,0 +1,78 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class EncryptionContrastList
+{
+	Map<Character, String> maplist = new HashMap<Character, String>();
+
+	public EncryptionContrastList()
+	{
+
+		maplist.put('A', "38");
+		maplist.put('B', "3B");
+		maplist.put('C', "3A");
+		maplist.put('D', "3D");
+		maplist.put('E', "3C");
+		maplist.put('F', "3F");
+		maplist.put('G', "3E");
+		maplist.put('H', "31");
+		maplist.put('I', "30");
+		maplist.put('J', "33");
+		maplist.put('K', "32");
+		maplist.put('L', "35");
+		maplist.put('M', "34");
+		maplist.put('N', "37");
+		maplist.put('O', "36");
+		maplist.put('P', "29");
+		maplist.put('Q', "28");
+		maplist.put('R', "2B");
+		maplist.put('S', "2A");
+		maplist.put('T', "2D");
+		maplist.put('U', "2C");
+		maplist.put('V', "2F");
+		maplist.put('W', "2E");
+		maplist.put('X', "21");
+		maplist.put('Y', "20");
+		maplist.put('Z', "23");
+		maplist.put('a', "18");
+		maplist.put('b', "1B");
+		maplist.put('c', "1A");
+		maplist.put('d', "1D");
+		maplist.put('e', "1C");
+		maplist.put('f', "1F");
+		maplist.put('g', "1E");
+		maplist.put('h', "11");
+		maplist.put('i', "10");
+		maplist.put('j', "13");
+		maplist.put('k', "12");
+		maplist.put('l', "15");
+		maplist.put('m', "14");
+		maplist.put('n', "17");
+		maplist.put('o', "16");
+		maplist.put('p', "09");
+		maplist.put('q', "08");
+		maplist.put('r', "0B");
+		maplist.put('s', "0A");
+		maplist.put('t', "0D");
+		maplist.put('u', "0C");
+		maplist.put('v', "0F");
+		maplist.put('w', "0E");
+		maplist.put('x', "01");
+		maplist.put('y', "00");
+		maplist.put('z', "03");
+		maplist.put('0', "49");
+		maplist.put('1', "48");
+		maplist.put('2', "4B");
+		maplist.put('3', "4A");
+		maplist.put('4', "4D");
+		maplist.put('5', "4C");
+		maplist.put('6', "4F");
+		maplist.put('7', "4E");
+		maplist.put('8', "41");
+		maplist.put('9', "40");
+		maplist.put('=', "44");
+		maplist.put('.', "57");
+
+	}
+
+}
